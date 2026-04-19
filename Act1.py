e3c8d7d3878d9ab0fd237d26c8ee62c23e1552e7
gnisimura <german.nisimura@tec.mx>
2026-04-18 22:16:48 -0600

    envia

---
+++ Act1.py
@@ -0,0 +1,4 @@
+# Centígrados a Fahrenheit
+centigrados = float(input("ingrese la temperatura en grados centigrados: "))
+fahrenheit = (centigrados* 9/5)+32
+print("La temperatura en grados Fahrenheit:", fahrenheit)

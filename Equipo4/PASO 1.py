df3a9bb548b57e1b3337fbcca81f0a7065c63d40
a00846091-design <a00846091@tec.mx>
2026-04-21 16:48:34 -0600

    Add files via upload

---
+++ Equipo4/PASO 1.py
@@ -0,0 +1,12 @@
+Python 3.13.13 (tags/v3.13.13:01104ce, Apr  7 2026, 19:25:48) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> 
+======================= RESTART: C:/Users/arand/PASO 1.py ======================
+¿Cuántos artículos vas a comprar? 3
+Nombre del artículo: Fruta
+Precio: 30
+Nombre del artículo: pan
+Precio: 10
+Nombre del artículo: Arroz
+Precio: 40
+Total a pagar: $ 80.0
